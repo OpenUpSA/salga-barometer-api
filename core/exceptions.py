18390810b0daaa648ac4b09baaf087f1fec398b3
lunga <stumenz.complex@gmail.com>
2018-03-13 15:01:15 +0200

    parameters exception

--- core/exceptions.py
+++ core/exceptions.py
@@ -5,3 +5,12 @@ class TooManyResultsException(APIException):
     status_code = 400
     default_detail = 'Too Many Results'
     default_code = 'too_many_results'
+
+
+class NotEnoughParameters(APIException):
+    """
+    Show when not all query parameters have been set.
+    """
+    status_code = 400
+    default_detail = 'Not Enough Parameters'
+    default_code = 'not_enough_parameters'
